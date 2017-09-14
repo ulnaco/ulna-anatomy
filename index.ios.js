63c566516ea3fa0382fcfad6889841ea0919d7b9
Terrillo Walls <hello@ulna.co>
2017-09-14 07:28:30 -0500

    Xcode update

--- index.ios.js
+++ index.ios.js
@@ -18,8 +18,8 @@ import {
   MXTrack,
 } from 'rn-mixpanel-lite';
 
-MXInit('5df62a939c9625dfeb091400575b6c7f')
-MXTrack('YO')
+// MXInit('5df62a939c9625dfeb091400575b6c7f')
+// MXTrack('YO')
 
 
 const anatomy = StackNavigator({
