600d248e35f7353ddc1916bf84e9db91a2e8decd
Terrillo Walls <hello@ulna.co>
2017-09-19 14:12:28 -0500

    Log weight

--- Views/DataCollection/LogWeight.js
+++ Views/DataCollection/LogWeight.js
@@ -3,18 +3,77 @@ import {
   View,
   Text,
   ScrollView,
-  TouchableHighlight
+  TouchableHighlight,
+  TextInput,
+  AlertIOS
 } from 'react-native';
+
+import AppleHealthkit from 'rn-apple-healthkit';
 
 import * as UL from 'ulna-ui'
 
 export class LogWeight extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: 0,
+      newWeight: 0
+    };
+  }
+
+  componentWillMount() {
+    AppleHealthkit.getLatestWeight({ unit: 'pound' }, (err: string, results: Object) => {
+      if (results) {
+        this.setState({
+          weight: results.value,
+          newWeight: results.value,
+        })
+      }
+    });
+
+  }
+
   render() {
     return (
       <ScrollView style={UL.ULStyles.window}>
         <View style={UL.ULStyles.screen}>
-          <UL.ULTitle text="LogWeight"/>
+          <TextInput
+            style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+            onChangeText={(text) => this.setState({newWeight: text})}
+            placeholder={this.state.weight.toString()}
+            keyboardType="numeric"
+            value={this.state.newWeight.toString()}
+          />
+          <TouchableHighlight
+             underlayColor='transparent'
+             onPress={() => {
+               var weight = this.state.newWeight
+               var options = { value: weight }
+               AlertIOS.alert(
+                 'Sync Complete',
+                 'Weight saved to Apple Health', [
+                   {text: 'Cancel', onPress: () => {
+                       const { navigate } = this.props.navigation;
+                       navigate('Weight')
+                     },
+                     style: 'cancel'
+                   },
+                   {text: 'OK', onPress: () => {
+                     AppleHealthkit.saveWeight(options: Object, (err: Object, results: Object) => {
+                       if (!err) {
+                         const { navigate } = this.props.navigation;
+                         navigate('Weight')
+                       }
+                     });
+                   }}
+                 ]
+               );
+             }}>
+              <View>
+                <UL.ULButton style="primary" text="Save" />
+              </View>
+          </TouchableHighlight>
         </View>
       </ScrollView>
     )
