e05c85c2485bbb5cdf9bd6ba13bafa41faed5f96
Terrillo Walls <hello@ulna.co>
2017-09-21 07:52:29 -0500

    Rating update :O

--- Tools/Algorithms/Rating.js
+++ Tools/Algorithms/Rating.js
@@ -18,23 +18,23 @@ export function rating(fn) {
           text: 'Considered Underweight'
         })
       }
-      if (result > 18.5 && result < 20) {
+      else if (result > 18.5 && result < 20) {
         score = 3
       }
-      if (result > 20 && result < 24.9) {
+      else if (result > 20 && result < 24.9) {
         explanation.push({
           type: 'good',
           text: 'Ideal height to weight ratio'
         })
         score = 5
       }
-      if (result > 25 && result < 26.9) {
+      else if (result > 25 && result < 26.9) {
         score = 4
       }
-      if (result > 27 && result < 29.9) {
+      else if (result > 27 && result < 29.9) {
         score = 2
       }
-      if (result > 30) {
+      else {
         score = 0
       }
       resolve(score)
@@ -60,21 +60,21 @@ export function rating(fn) {
           text: 'Recommended you walk +2000 more steps a day'
         })
       }
-      if ((total/5) > 5000) {
+      else if ((total/5) > 5000) {
         fitnessScore = 3
         explanation.push({
           type: 'ok',
           text: 'Recommended you walk +1000 more steps a day'
         })
       }
-      if ((total/5) > 8000) {
+      else if ((total/5) > 8000) {
         fitnessScore = 4
         explanation.push({
           type: 'good',
           text: 'Recommended daily steps on point'
         })
       }
-      if ((total/5) > 10000) {
+      else if ((total/5) > 10000) {
         fitnessScore = 5
         explanation.push({
           type: 'good',
