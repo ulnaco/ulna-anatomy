bcd1d77da3ef8e211562e05cdcff6cd5c49d33a5
ulnaco <31868372+ulnaco@users.noreply.github.com>
2017-09-28 17:31:09 -0500

    1.0.0-beta2 (#3)
    * Removed old logo assets
    
    * Build number update
    
    * Dependency update
    
    * Missing UI updates

--- Tools/Charts/BarVertical.js
+++ Tools/Charts/BarVertical.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BarChart } from 'react-native-charts'
 
-import * as UL from 'ulna-ui'
+import * as UI from '../../UI'
 
 export class BarVertical extends React.Component {
   constructor(props) {
@@ -14,13 +14,13 @@ export class BarVertical extends React.Component {
       <BarChart
         dataSets={[
           {
-            fillColor: UL.ULStyleguide.color.primaryLite,
+            fillColor: UI.UIStyleguide.color.primaryLite,
             data: [
               { value: this.props.values[0] },
             ]
           },
           {
-            fillColor: UL.ULStyleguide.color.primary,
+            fillColor: UI.UIStyleguide.color.primary,
             data: [
               { value: this.props.values[1] },
             ]
@@ -31,8 +31,8 @@ export class BarVertical extends React.Component {
         showGrid={false}
         barSpacing={5}
         style={{
-          height: UL.ULStyleguide.spacing * 10,
-          margin: UL.ULStyleguide.spacing,
+          height: UI.UIStyleguide.spacing * 10,
+          margin: UI.UIStyleguide.spacing,
         }} />
     )
   }
--- ulna-ui.sh
+++
@@ -1,2 +0,0 @@
-rm -rf node_modules/ulna-ui
-cp -R ../ulna-ui node_modules/ulna-ui
