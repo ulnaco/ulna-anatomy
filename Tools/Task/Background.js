ce52a23418318d2fbb6c2f0598a09edcc820ef3e
Ulna <hello@ulna.co>
2017-10-05 13:53:07 -0500

    Another background test

--- Tools/Task/Background.js
+++ Tools/Task/Background.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
-import { AppState, Text } from 'react-native'
+import {
+  AppState,
+} from 'react-native'
 import PushNotification from 'react-native-push-notification';
 import moment from 'moment'
 import AppleHealthkit from 'rn-apple-healthkit';
@@ -14,9 +16,48 @@ export function Background(view) {
 
   // Start Fetch
   BackgroundFetch.configure({
-     stopOnTerminate: false
-   }, function() {
-     T.Track('notification', 'BackgroundFetch')
-   })
+    minimumFetchInterval: 30,
+    stopOnTerminate: false,
+  }, function() {
+
+    // Steps
+    AppleHealthkit.initHealthKit({}: Object, (err: Object, results: Object) => {
+      AppleHealthkit.isAvailable((err: Object, available: boolean) => {
+        if (available) {
+          AppleHealthkit.getStepCount(null, (err: string, results: Object) => {
+            if (results) {
+              T.Track('notification', 'BackgroundFetch2', {
+                steps: results.value
+              })
+            }
+          });
+        }
+      });
+    });
+
+    BackgroundFetch.finish();
+  })
+
+  BackgroundFetch.status(function(status) {
+    switch(status) {
+      case BackgroundFetch.STATUS_AVAILABLE:
+        // Steps
+        AppleHealthkit.initHealthKit({}: Object, (err: Object, results: Object) => {
+          AppleHealthkit.isAvailable((err: Object, available: boolean) => {
+            if (available) {
+              AppleHealthkit.getStepCount(null, (err: string, results: Object) => {
+                if (results) {
+                  T.Track('notification', 'BackgroundFetch2', {
+                    steps: results.value,
+                    status: 'active'
+                  })
+                }
+              });
+            }
+          });
+        });
+        break;
+    }
+  });
 
 }
